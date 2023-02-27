4150962b86a4918967abf846393779fc4a2ec9c7
Kapusta <91116965+DyhaCreator@users.noreply.github.com>
2023-02-27 10:39:03 +0500

    Add files via upload

---
+++ tests/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test a = new Test(10,10);
+        }
+    }
+    public class Test
+    {
+        int x, y;
+        public Test(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ tests/Test/Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10");
+            Console.ReadLine();
+        }
+    }
+}
